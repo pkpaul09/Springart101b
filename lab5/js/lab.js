d82179b70341da3d024d834ebabe923ab9db3647
pkpaul09 <pkim22@ucsc.edu>
2024-04-25 22:22:23 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,8 +1,16 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 5
+// Author: Paul Kim
+// Date: 25 April 2024
 
 // Constants
+
+make = "Honda";
+model = "Accord";
+color = "Silver";
+year = 2015;
+age = 2024 - year;
+
+document.writeln("Make: " + make + "<br>Model: " + model + "<br>Color: " + color + "<br>Year: " + year + "<br>Age: " + age);
 
 // Functions
 
