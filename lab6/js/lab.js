4c4baf739929873e9ff603c59ef77f25aac816c6
pkpaul09 <pkim22@ucsc.edu>
2024-04-28 18:48:08 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,8 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//Lab 6
+// Author: Paul Kim
+// Date:28 April 2024
 
 // Constants
+
+myTransport = ["Bus", "Car", "Walking", "Uber", "Lyft"];
+myMainRide = {
+  make: "Honda",
+  model: "Accord",
+  color: "Silver",
+  year: 2015,
+  age: function() {
+return 2024 - this.year;
+  }
+}
+
+
+
+document.writeln("Getting around: " + myTransport + "<br>");
+
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre");
 
 // Functions
 
